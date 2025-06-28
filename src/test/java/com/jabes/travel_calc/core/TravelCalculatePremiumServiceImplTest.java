16bbc75eeea4cd9942b6cb13364b56acb53f79e9
jabes <jabesgames@gmail.com>
2025-06-28 14:59:17 +0300

    task 014 - OK

--- src/test/java/com/jabes/travel_calc/core/TravelCalculatePremiumServiceImplTest.java
+++ src/test/java/com/jabes/travel_calc/core/TravelCalculatePremiumServiceImplTest.java
@@ -14,22 +14,49 @@ class TravelCalculatePremiumServiceImplTest {
     private final TravelCalculatePremiumServiceImpl calculate = new TravelCalculatePremiumServiceImpl();
 
     @Test
-    public void testCalculatePremium_ReturnsCorrectResponse() {
-
+    public void testCalculatePremium_ReturnsCorrectFirstName() {
         String firstNameExpected = "Иван";
-        String lastNameExpected = "Петров";
-        Date dateFromExpected = new Date(2025, 6, 1);
-        Date dateToExpected = new Date(2025, 6, 9);
-
-        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest(firstNameExpected, lastNameExpected, dateFromExpected, dateToExpected);
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest(
+                firstNameExpected, null, null, null);
 
         TravelCalculatePremiumResponse response = calculate.calculatePremium(request);
 
         Assertions.assertEquals(firstNameExpected, response.getPersonFirstName(), "Имена должны совпадать.");
-        Assertions.assertEquals(lastNameExpected, request.getPersonLastName(), "Фамилии должны совпадать.");
-        Assertions.assertEquals(dateFromExpected, request.getAgreementDateFrom(), "dateFrom не совпадают.");
-        Assertions.assertEquals(dateToExpected, request.getAgreementDateTo(), "dateTo не совпадает.");
+    }
 
+    @Test
+    public void testCalculatePremium_ReturnsCorrectLastName() {
+        String lastNameExpected = "Петров";
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest(
+                null, lastNameExpected, null, null);
+
+        TravelCalculatePremiumResponse response = calculate.calculatePremium(request);
+
+        Assertions.assertEquals(lastNameExpected, request.getPersonLastName(), "Фамилии должны совпадать.");
+    }
+
+    @Test
+    public void testCalculatePremium_ReturnsCorrectDateFrom() {
+        Date dateFromExpected = new Date(2025, 6, 1);
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest(
+                null, null, dateFromExpected, null);
+
+        TravelCalculatePremiumResponse response = calculate.calculatePremium(request);
+
+        Assertions.assertEquals(dateFromExpected, request.getAgreementDateFrom(), "dateFrom не совпадают.");
+
+    }
+
+    @Test
+    public void testCalculatePremium_ReturnsCorrectDateTo() {
+        Date dateToExpected = new Date(2025, 6, 9);
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest(
+                null, null, null, dateToExpected);
+
+        TravelCalculatePremiumResponse response = calculate.calculatePremium(request);
+
+
+        Assertions.assertEquals(dateToExpected, request.getAgreementDateTo(), "dateTo не совпадает.");
     }
 
 }
