e403de58c14cffda7d2368cffa57a2a8d423da35
jabes <jabesgames@gmail.com>
2025-06-28 12:35:17 +0300

    qq

---
+++ src/test/java/com/jabes/travel_calc/TravelCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jabes.travel_calc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
