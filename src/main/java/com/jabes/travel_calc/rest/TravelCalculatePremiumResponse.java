961087e2b2b5f322cde63e620a421a80ed5473b0
jabes <jabesgames@gmail.com>
2025-06-28 13:51:24 +0300

    task 011 - OK

--- src/main/java/com/jabes/travel_calc/rest/TravelCalculatePremiumResponse.java
+++ src/main/java/com/jabes/travel_calc/rest/TravelCalculatePremiumResponse.java
@@ -1,7 +1,12 @@
 package com.jabes.travel_calc.rest;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Getter
+@Setter
 public class TravelCalculatePremiumResponse {
 
     private String personFirstName;
@@ -9,5 +14,13 @@ public class TravelCalculatePremiumResponse {
     private Date agreementDateFrom;
     private Date agreementDateTo;
 
+    public TravelCalculatePremiumResponse() {
+    }
 
+    public TravelCalculatePremiumResponse(String personFirstName, String personLastName, Date agreementDateFrom, Date agreementDateTo) {
+        this.personFirstName = personFirstName;
+        this.personLastName = personLastName;
+        this.agreementDateFrom = agreementDateFrom;
+        this.agreementDateTo = agreementDateTo;
+    }
 }
